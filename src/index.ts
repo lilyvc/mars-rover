8496dbdd2b9566701ec91e9e85c56558735a140a
Lily Cooksley <lilycooksley@gmail.com>
2024-12-05 10:41:14 +0000

    simplify entrypoint

--- src/index.ts
+++ src/index.ts
@@ -9,36 +9,31 @@ import { calculateState } from "./utils/calculateState";
 import { parseInput } from "./utils/parseInput";
 import { Grid, State, Robot } from "./shared.types";
 
-export const processAllRobots = (input: string): string => {
+const inputArray = [
+  `4 8 
+(2, 3, E) LFRFF 
+(0, 2, N) FFLFRFF`,
+  `4 8 
+(2, 3, N) FLLFR 
+(1, 0, S) FFRLF`,
+  ``,
+];
+
+const output = inputArray.map((input) => processAllRobots(input)).join("\n");
+console.log(output);
+
+export function processAllRobots(input: string): string {
   const { grid, robots } = parseInput(input);
   const results = robots.map((robot: Robot) => processRobot(robot, grid));
   return results.join("\n");
-};
+}
 
-const processRobot = (robot: Robot, grid: Grid): string => {
+function processRobot(robot: Robot, grid: Grid): string {
   const { initialState, commands } = robot;
   const { finalState, lost } = calculateState(initialState, commands, grid);
   return formatResult(finalState, lost);
-};
-
-const formatResult = (State: State, lost: boolean): string => {
-  return `(${State.x}, ${State.y}, ${State.orientation})${lost ? " LOST" : ""}`;
-};
-
-function main() {
-  const inputArray = [
-    `4 8 
-(2, 3, E) LFRFF 
-(0, 2, N) FFLFRFF`,
-    `4 8 
-(2, 3, N) FLLFR 
-(1, 0, S) FFRLF`,
-    ``,
-  ];
-  const output = inputArray.map((input) => processAllRobots(input)).join("\n");
-  console.log(output);
 }
 
-if (require.main === module) {
-  main();
+function formatResult(State: State, lost: boolean): string {
+  return `(${State.x}, ${State.y}, ${State.orientation})${lost ? " LOST" : ""}`;
 }
